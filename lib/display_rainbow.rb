48d033436dd3b7616c9d384cb4da9a04d1c9bf59
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-12 01:39:40 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(colors)
-colors = 
-puts "#{array of colors}"
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+puts "R: #{[0]}, O: #{1}, Y: #{2}, G: #{[3]}
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
