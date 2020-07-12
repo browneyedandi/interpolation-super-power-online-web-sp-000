69fe0aec344f6c0c983f5008134c2a29c8a47cb5
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-12 01:23:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,2 @@
-def display_rainbow(colors)
-   puts "#{R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet}"
-  colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-  end
+def display_rainbow
 
