ecb2caabae8256bb40f5387162324aae3c066d4d
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-12 01:30:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+def display_rainbow([c1,c2,c3,c4,c5,c6,c7])
 end
 
 
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
