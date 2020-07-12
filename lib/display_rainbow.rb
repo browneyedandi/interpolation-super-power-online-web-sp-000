ea54100a6d3909ca07bdcad1be8352c85482d10b
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-12 01:34:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
-def display_rainbow([c1,c2,c3,c4,c5,c6,c7])
-end
-
-
+def display_rainbow(colors)
+colors = 
+puts "#{array of colors}"
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
